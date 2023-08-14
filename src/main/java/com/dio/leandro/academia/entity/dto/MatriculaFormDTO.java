385e263b56bda91639655e70220be602e755a2d5
Leandro Rocha <lerocha644@gmail.com>
2023-08-13 22:21:35 -0300

    Validation for MatriculaFormDTO

--- src/main/java/com/dio/leandro/academia/entity/dto/MatriculaFormDTO.java
+++ src/main/java/com/dio/leandro/academia/entity/dto/MatriculaFormDTO.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class MatriculaFormDTO {
 
+    @NotNull(message = "Preencha o campo corretamente.")
+    @Positive(message = "O Id do aluno precisa ser positivo.")
     private Long idAluno;
 }
